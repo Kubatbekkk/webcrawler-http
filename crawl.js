86f52afa26190fd855db23cb4aee124b8829a100
Kubatbekkk <kubineccastro@gmail.com>
2023-03-06 19:20:34 +0600

    feat: getUrlsFromHtml and its tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for(let linkElement of linkElements) {
+        if(linkElement.href.slice(0, 1) === '/') {
+            //relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.error(`Error with relative url: ${error.message}`)
+            }
+        } else {
+            //absolute
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.error(`Error with absolute url: ${error.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString) {
     const urlObject = new URL(urlString)
     const hostPath = `${urlObject.hostname}${urlObject.pathname}`
@@ -8,5 +36,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
